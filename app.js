d0918255e9d2099eab6b68a441d5f93e83c28041
Matías Menich <matimenich@gmail.com>
2016-03-16 12:24:24 -0300

    Fixing static dependencies

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+'use strict';
+
 var serveStatic = require('serve-static');
 var server = require('./server/server')();
 
 server.set('views', './dist');
-server.use(serveStatic('dist', {
-    'index': ['index.html']
-}));
+server.use(serveStatic('dist'));
 
 server.listen(server.get('port'), function () {
     console.log('✔ Express server listening connected listening on: ' + server.get('port'));
